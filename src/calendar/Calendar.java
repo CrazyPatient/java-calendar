5cbeccef862c78c5b1c15021917ee7556147be77
hhkktt <hhkktt@121.164.44.8>
2022-04-23 13:55:22 +0900

    Fix encoding

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -4,8 +4,5 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Calendar");
-		System.out.println("일\t월\t화\t수\t목\t금\t토");
-		System.out.println("-------------------------------------------------");
-		System.out.println("1\t2\t3\t4\t5\t6\t7");
 	}
 }
